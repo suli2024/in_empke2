2524355d2622bd2f9b30f65350dfea3ce3674d7b
Erős István <ers@zold.lan>
2025-04-02 12:18:44 +0200

    Dolgozók Create művelet kész

---
+++ web/src/app.js
@@ -0,0 +1,33 @@
+const tbody = document.querySelector('#tbody')
+
+//Read
+const url = 'http://localhost:8000/api/employees'
+
+//Promise
+
+fetch(url)
+.then((response) => {
+  return response.json()
+})
+.then((result) => {
+  console.log(result)  
+  renderTbody(result.data)
+});
+
+function renderTbody(empList) {
+  var tbodyContent = '';
+  empList.forEach((emp) => {
+    var row = `
+    <tr>
+      <td>${emp.id}</td>      
+      <td>${emp.name}</td>      
+      <td>${emp.city}</td>      
+      <td>${emp.salary}</td>
+    </tr>
+    `;    
+    tbodyContent += row;
+  })
+  tbody.innerHTML = tbodyContent
+  
+
+}
